3dd952ab39cf833c3b4737fa5cbb38e24ee379f8
Shuokai Ma <kkm981104@gmail.com>
2022-10-30 21:48:17 +0100

    Minor changes in CPUTop.scala

--- src/main/scala/CPUTop.scala
+++ src/main/scala/CPUTop.scala
@@ -51,18 +51,18 @@ class CPUTop extends Module {
   // RegisterFile -> ALU
   alu.io.oper1:=registerFile.io.a
   // Extend sign for instruction(15,0) from 16 to 32 bits
-  val signExtend=Wire(UInt(32.W))
+  val signExtension=Wire(UInt(32.W))
 
-  signExtend:=Cat(Fill(16,0.U),programMemory.io.instructionRead(15,0))
-  alu.io.oper2:=Mux(controlUnit.io.aluSrc,signExtend,registerFile.io.b)
+  signExtension:=Cat(Fill(16,0.U),programMemory.io.instructionRead(15,0))
+  alu.io.oper2:=Mux(controlUnit.io.aluSrc,signExtension,registerFile.io.b)
   alu.io.sel:=controlUnit.io.aluOp
 
   // Reduce sign four mux output from 32 to 16 bits
-  val signReduce=Wire(UInt(16.W))
-  signReduce:=Mux(controlUnit.io.loadFromMemory,registerFile.io.a,Mux(controlUnit.io.aluSrc,signExtend,registerFile.io.b))
+  val signReduction=Wire(UInt(16.W))
+  signReduction:=Mux(controlUnit.io.loadFromMemory,registerFile.io.a,Mux(controlUnit.io.aluSrc,signExtension,registerFile.io.b))
 
   // RegisterFile -> DataMemory
-  dataMemory.io.address:=signReduce
+  dataMemory.io.address:=signReduction
   dataMemory.io.dataWrite:=registerFile.io.a
   dataMemory.io.writeEnable:=controlUnit.io.writeToMemory
 
